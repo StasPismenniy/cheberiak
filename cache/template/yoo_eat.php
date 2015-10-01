77757d12c4190675fddbfcae9d789f456ba9cce9
stas <stas@ekreative.com>
2015-10-01 23:08:51 +0300

    first commit

---
+++ cache/template/yoo_eat.php
@@ -0,0 +1,6 @@
+{
+    "tmpl_check": "e75e755750cf459a09e9bd84320c5eba",
+    "tmpl_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/themes\\\/downloads\\\" target=\\\"_blank\\\">Download Eat Theme 1.0.8<\\\/a>\"}",
+    "warp_check": "518089c00f3b1b32ed8c7834190fe9ca",
+    "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.12<\\\/a>\"}"
+}

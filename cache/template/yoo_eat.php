7d9180049de3e48f493a0efa3ab37bdc7af92107
stas <stas@ekreative.com>
2015-10-12 22:04:48 +0300

    bt content slider

--- cache/template/yoo_eat.php
+++ cache/template/yoo_eat.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "c35931a2e6caf9b947b3688d3294de36",
+    "tmpl_check": "8d8fbbacd80543b4b8db6b655abe93f0",
     "tmpl_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/themes\\\/downloads\\\" target=\\\"_blank\\\">Download Eat Theme 1.0.8<\\\/a>\"}",
-    "warp_check": "a57b759d6d1a9f78fdbff0fdbf9997bd",
+    "warp_check": "e9fc4e3efc74dba0863921967c2cd322",
     "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.12<\\\/a>\"}"
 }

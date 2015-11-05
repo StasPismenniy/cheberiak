3046700b49123f6ce83b9778e84c0914b1643a28
stas <stas@ekreative.com>
2015-11-05 23:04:59 +0200

    rs form styles + new joomla

--- cache/template/yoo_eat.php
+++ cache/template/yoo_eat.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "3114c042d2ffc76fd745ed8f2ea0d0c5",
+    "tmpl_check": "c74ab2c814e00ed095b97b3c48b484e3",
     "tmpl_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/themes\\\/downloads\\\" target=\\\"_blank\\\">Download Eat Theme 1.0.8<\\\/a>\"}",
-    "warp_check": "f958689fb484dfc0f1256f86ef2fc142",
+    "warp_check": "9d8fa7e90685333b411ab441db995963",
     "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.14<\\\/a>\"}"
 }

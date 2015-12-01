ca915427ae5698f3099f7586a32e8b417e8219bd
Stas <stas@ekreative.com>
2015-12-01 17:38:15 +0200

    new pages + styles

--- cache/template/yoo_eat.php
+++ cache/template/yoo_eat.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "941c07ba052af3a885fdc3da74d26583",
+    "tmpl_check": "0d50158e89fc2ee32d54540c493a75f3",
     "tmpl_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/themes\\\/downloads\\\" target=\\\"_blank\\\">Download Eat Theme 1.0.8<\\\/a>\"}",
-    "warp_check": "514c8ba1c7ed61417336b3a93c2c106d",
+    "warp_check": "c418451ece5cfdb54d4b36d81e40e7fa",
     "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.14<\\\/a>\"}"
 }

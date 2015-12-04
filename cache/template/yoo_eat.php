ec2fd0da4bfb5304a0ecc4a5b8105af59eaeeda0
Stas <stas@ekreative.com>
2015-12-04 16:58:45 +0200

    projects paddings

--- cache/template/yoo_eat.php
+++ cache/template/yoo_eat.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "c85ba5105278a62ec66a4c318c856043",
-    "tmpl_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/themes\\\/downloads\\\" target=\\\"_blank\\\">Download Eat Theme 1.0.8<\\\/a>\"}",
-    "warp_check": "004f0075cc0e200f66b7bc1d2a68212d",
-    "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.14<\\\/a>\"}"
+    "tmpl_check": "83bdf1d87d45af98e2e64b3c7803f8e8",
+    "tmpl_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/themes\\\/downloads\\\" target=\\\"_blank\\\">Download Eat Theme 1.0.8<\\\/a>\",\"version\":\"1.0.8\"}",
+    "warp_check": "4a5138be9a99f3c74954fa5c998e1295",
+    "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.15<\\\/a>\",\"version\":\"7.3.15\"}"
 }
